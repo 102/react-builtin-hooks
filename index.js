279e998ba3d09921097fdb9eee92fa9eb0314fd2
Roman Gusev <rgusev2@gmail.com>
2019-09-19 23:19:11 +0600

    v0.1.6

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-export {
-  useContext,
-  useState,
-  useReducer,
-  useRef,
-  useDebugValue,
-  useEffect,
-  useLayoutEffect,
-  useCallback,
-  useMemo,
-  useImperativeHandle
-} from "react";
+const React = require("react");
+
+module.exports = {
+  useContext: React.useContext,
+  useState: React.useState,
+  useReducer: React.useReducer,
+  useRef: React.useRef,
+  useDebugValue: React.useDebugValue,
+  useEffect: React.useEffect,
+  useLayoutEffect: React.useLayoutEffect,
+  useCallback: React.useCallback,
+  useMemo: React.useMemo,
+  useImperativeHandle: React.useImperativeHandle
+};
